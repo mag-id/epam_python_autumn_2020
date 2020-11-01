177e323d2c9777c3f5fe77081ee5aec13376f637
mag <m.a.gazimagomedov@gmail.com>
2020-11-02 01:44:12 +0300

    Docstrings added and variables refactored.

--- homework_1/sample_project/calculator/calc.py
+++ homework_1/sample_project/calculator/calc.py
@@ -1,2 +1,8 @@
-def check_power_of_2(a: int) -> bool:
-    return not (bool(a & (a - 1)))
+"""
+Calculator functions.
+"""
+
+
+def check_power_of_2(value: int) -> bool:
+    """Returns `True` if `value` is power of 2 else `False`."""
+    return not bool(value & (value - 1)) if value != 0 else False
