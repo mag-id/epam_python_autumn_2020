3d79a545caeac044af755ad1bbcb2f3567da0b8c
mag <m.a.gazimagomedov@gmail.com>
2020-11-02 01:46:48 +0300

    Docstrings added, variables refactored.

--- homework_1/sample_project/tests/test_calculator.py
+++ homework_1/sample_project/tests/test_calculator.py
@@ -1,6 +1,10 @@
+"""
+Unit tests for `calculator` module.
+"""
+
 import pytest
 
-from calculator.calc import check_power_of_2
+from homework_1.sample_project.calculator.calc import check_power_of_2
 
 
 @pytest.mark.parametrize(
@@ -11,6 +15,7 @@ from calculator.calc import check_power_of_2
     ],
 )
 def test_power_of_2(value: int, expected_result: bool):
+    """Pass test if result of `value` is equal to `expected_result`."""
     actual_result = check_power_of_2(value)
 
     assert actual_result == expected_result
