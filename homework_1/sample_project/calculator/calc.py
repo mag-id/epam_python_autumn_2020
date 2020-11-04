61901555ed602e05f5ad8b09f9085cc19e5094b8
mag <m.a.gazimagomedov@gmail.com>
2020-11-04 14:47:34 +0300

    Docstring and `check_power_of_2` expression improved.

--- homework_1/sample_project/calculator/calc.py
+++ homework_1/sample_project/calculator/calc.py
@@ -1,8 +1,8 @@
 """
-Calculator functions.
+Functions of the `sample_project.calculator.calc` module.
 """
 
 
 def check_power_of_2(value: int) -> bool:
     """Returns `True` if `value` is power of 2 else `False`."""
-    return not bool(value & (value - 1)) if value != 0 else False
+    return not bool(value & (value - 1)) if value > 0 else False
