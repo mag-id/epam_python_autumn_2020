ef72c7e12bd630415533ad0953b63692b621aafa
mag <m.a.gazimagomedov@gmail.com>
2020-11-04 14:53:07 +0300

    Docstring and test cases improved.

--- homework_1/sample_project/tests/test_calculator.py
+++ homework_1/sample_project/tests/test_calculator.py
@@ -10,18 +10,22 @@ from homework_1.sample_project.calculator.calc import check_power_of_2
 @pytest.mark.parametrize(
     ["value", "expected_result"],
     [
-        (-2, False),
-        (-1, False),
-        (-0, False),
-        (0, False),
-        (1, True),
-        (2, True),
-        (12, False),
-        (65536, True),
+        pytest.param(-65536, False, id="Negative case: -65536 is not a power of 2."),
+        pytest.param(-12, False, id="Negative case: -12 is not a power of 2."),
+        pytest.param(-2, False, id="Negative case: -2 is not a power of 2."),
+        pytest.param(-1, False, id="Negative case: -1 is not a power of 2."),
+        pytest.param(0, False, id="Boundary case: 0 is not a power of 2."),
+        pytest.param(1, True, id="Boundary case: 2**0 = 1."),
+        pytest.param(2, True, id="Simple case: 2**1 = 2."),
+        pytest.param(12, False, id="Simple case: 12 is not a power of 2."),
+        pytest.param(65536, True, id="Simple case: 2**16 = 65536."),
     ],
 )
 def test_power_of_2(value: int, expected_result: bool):
-    """Pass test if result of `value` is equal to `expected_result`."""
+    """
+    Passes test if `check_power_of_2`(`value`)
+    is equal to `expected_result`.
+    """
     actual_result = check_power_of_2(value)
 
     assert actual_result == expected_result
