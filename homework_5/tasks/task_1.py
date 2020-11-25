75d5ac1c4d7141e93b69e368bc7550a672225b48
mag <m.a.gazimagomedov@gmail.com>
2020-11-26 01:01:57 +0300

    updated

--- homework_5/tasks/task_1.py
+++ homework_5/tasks/task_1.py
@@ -188,24 +188,3 @@ class Teacher:
     def create_homework(text: str, deadline: int) -> Homework:
         """Returns `Homework` instance with `text` and `deadline`."""
         return Homework(text, deadline)
-
-
-if __name__ == "__main__":
-
-    teacher = Teacher("Daniil", "Shadrin")
-    student = Student("Roman", "Petrov")
-    print(teacher.last_name)  # Daniil
-    print(student.first_name)  # Petrov
-
-    expired_homework = teacher.create_homework("Learn functions", 0)
-    print(expired_homework.created)  # Example: 2019-05-26 16:44:30.688762
-    print(expired_homework.deadline)  # 0:00:00
-    print(expired_homework.text)  # 'Learn functions'
-
-    # create function from method and use it
-    create_homework_too = teacher.create_homework
-    oop_homework = create_homework_too("create 2 simple classes", 5)
-    print(oop_homework.deadline)  # 5 days, 0:00:00
-
-    student.do_homework(oop_homework)
-    student.do_homework(expired_homework)  # You are late
