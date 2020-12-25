e69f0c81df55f2cceeb06b7430e3456273f61437
mag <m.a.gazimagomedov@gmail.com>
2020-12-25 17:42:01 +0300

    Fixture and related naming refactored.

--- homework_9/tests/test_1.py
+++ homework_9/tests/test_1.py
@@ -8,78 +8,85 @@ import pytest
 
 from homework_9.tasks.task_1 import merge_sorted_files
 
-FIRST_CONTENT = "1\n3\n5\n"
-SECOND_CONTENT = "2\n4\n6"
-THIRD_CONTENT = "\n\n\n"
-FOURTH_CONTENT = ""
+ONE_THREE_FIVE_LINES = "1\n3\n5\n"
+TWO_FOUR_SIX_LINES = "2\n4\n6"
+THREE_EMPTY_LINES = "\n\n\n"
+EMPTY = ""
 
 
 @pytest.fixture
-def test_files(tmp_path):
+def test_files_creator(tmp_path):
     """
     Creates file paths to the:
 
-    + "first_file.txt" with `FIRST_CONTENT`
-    + "second_file.txt" with `SECOND_CONTENT`
-    + "third_file.py" with `THIRD_CONTENT`
-    + "fourth_file.txt" with `FOURTH_CONTENT`
+    + "one_three_five_lines.txt" with `ONE_THREE_FIVE_LINES`
+    + "two_four_six_lines.txt" with `TWO_FOUR_SIX_LINES`
+    + "three_empty_lines.py" with `THREE_EMPTY_LINES`
+    + "empty.py" with `EMPTY`
     """
     test_dir = tmp_path / "tmp_dir"
     test_dir.mkdir()
 
-    first_file = test_dir / "first_file.txt"
-    second_file = test_dir / "second_file.txt"
-    third_file = test_dir / "third_file.py"
-    fourth_file = test_dir / "fourth_file.py"
+    one_three_five_lines_txt = test_dir / "one_three_five_lines.txt"
+    two_four_six_lines_txt = test_dir / "two_four_six_lines.txt"
+    three_empty_lines_py = test_dir / "three_empty_lines.py"
+    empty_py = test_dir / "empty.py"
 
     file_content = {
-        first_file: FIRST_CONTENT,
-        second_file: SECOND_CONTENT,
-        third_file: THIRD_CONTENT,
-        fourth_file: FOURTH_CONTENT,
+        one_three_five_lines_txt: ONE_THREE_FIVE_LINES,
+        two_four_six_lines_txt: TWO_FOUR_SIX_LINES,
+        three_empty_lines_py: THREE_EMPTY_LINES,
+        empty_py: EMPTY,
     }
 
     for file, content in file_content.items():
         file.write_text(content)
 
-    return first_file, second_file, third_file, fourth_file
+    return (
+        one_three_five_lines_txt,
+        two_four_six_lines_txt,
+        three_empty_lines_py,
+        empty_py,
+    )
 
 
 # pylint: disable=redefined-outer-name
 
 
-def test_single_file(test_files):
+def test_single_file(test_files_creator):
     """
     Passes test if `merge_sorted_files` works correctly
     on the one sorted file with integers per lines.
     """
-    first_file, _, _, _ = test_files
-    assert list(merge_sorted_files([first_file])) == [1, 3, 5]
+    one_three_five_lines_txt, _, _, _ = test_files_creator
+    assert list(merge_sorted_files([one_three_five_lines_txt])) == [1, 3, 5]
 
 
-def test_multiple_files(test_files):
+def test_multiple_files(test_files_creator):
     """
     Passes test if `merge_sorted_files` works correctly
     on the sorted files with integers per lines.
     """
-    first_file, second_file, _, _ = test_files
-    result = merge_sorted_files([first_file, Path(second_file)])
+    one_three_five_lines_txt, two_four_six_lines_txt, _, _ = test_files_creator
+    result = merge_sorted_files(
+        [one_three_five_lines_txt, Path(two_four_six_lines_txt)]
+    )
     assert list(result) == [1, 2, 3, 4, 5, 6]
 
 
-def test_empty_lines_file(test_files):
+def test_empty_lines_file(test_files_creator):
     """
     Passes test if `merge_sorted_files` yields
     nothing for the file with empty lines.
     """
-    _, _, third_file, _ = test_files
-    assert list(merge_sorted_files([third_file])) == []
+    _, _, three_empty_lines_py, _ = test_files_creator
+    assert list(merge_sorted_files([three_empty_lines_py])) == []
 
 
-def test_empty_file(test_files):
+def test_empty_file(test_files_creator):
     """
     Passes test if `merge_sorted_files`
     yields nothing for the empty file.
     """
-    _, _, _, fourth_file = test_files
-    assert list(merge_sorted_files([fourth_file])) == []
+    _, _, _, empty_py = test_files_creator
+    assert list(merge_sorted_files([empty_py])) == []
