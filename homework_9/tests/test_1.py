23450b8310523fbf36e1515ebb1449590945e523
mag <m.a.gazimagomedov@gmail.com>
2020-12-27 19:19:23 +0300

    Test for repeated integers and unsorted integers were added.

--- homework_9/tests/test_1.py
+++ homework_9/tests/test_1.py
@@ -87,3 +87,28 @@ def test_empty_file(create_tmp_file):
     """
     empty_txt = create_tmp_file("empty.txt", "")
     assert list(merge_sorted_files([empty_txt])) == []
+
+
+def test_repeated_integers(create_tmp_file):
+    """
+    Passes test if `merge_sorted_files`
+    correctly merges repeated integers.
+    """
+    _1_3_5_lines_txt = create_tmp_file("1_3_5_lines.txt", "1\n3\n5\n")
+    _2_5_6_lines_txt = create_tmp_file("2_5_6_lines.txt", "2\n5\n6\n")
+    files = [_1_3_5_lines_txt, _2_5_6_lines_txt]
+    assert list(merge_sorted_files(files)) == [1, 2, 3, 5, 5, 6]
+
+
+def test_unsorted_integers(create_tmp_file):
+    """
+    Passes test if `merge_sorted_files` can
+    not correctly merge unsorted integers.
+    """
+    _5_3_1_lines_txt = create_tmp_file("5_3_1_lines.txt", "5\n3\n1\n")
+    _2_6_4_lines_txt = create_tmp_file("2_6_4_lines.txt", "2\n6\n4\n")
+
+    files = [_5_3_1_lines_txt, _2_6_4_lines_txt]
+    result = list(merge_sorted_files(files))
+    assert result != [1, 2, 3, 4, 5, 6]
+    assert result == [2, 5, 3, 1, 6, 4]
