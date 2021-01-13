c4023bcc1be4d605564df5be3ce06c9ddee51f0e
mag <m.a.gazimagomedov@gmail.com>
2021-01-14 01:35:40 +0300

    refactored

--- homework_10/stocks/main.py
+++ homework_10/stocks/main.py
@@ -1,179 +1,63 @@
-"""The entry point for `StocksParser` and data collecting."""
+"""`StocksParser` entry point."""
 
-from json import dump, load
-from parser import StocksParser  # pylint: disable=no-name-in-module
 from pathlib import Path
-from typing import Callable, Dict, List, Tuple
 
-__all__ = ["main"]
-
-CODE = "code"
-NAME = "name"
-PRICE = "price"
-RATIO = "P/E"
-GROWTH = "growth"
-PROFIT = "potential profit"
-
-ALL_DATA_STOCKS = "all_data_stocks.json"
-TOP_TEN_EXPENSIVE_STOCKS = "top_ten_expensive_stocks.json"
-TOP_TEN_LOW_RATIO_STOCKS = "top_ten_low_ratio_stocks.json"
-TOP_TEN_GROWTHING_STOCKS = "top_ten_growthing_stocks.json"
-TOP_TEN_PROFITING_STOCKS = "top_ten_profiting_stocks.json"
+from stocks_parser import StocksParser
 
 
-def main(path: str, encoding="UTF-8"):
+def main(  # pylint: disable=too-many-arguments
+    directory: str,
+    top_low_ratio_stocks: str = "top_low_ratio_stocks.json",
+    top_expensive_stocks: str = "top_expensive_stocks.json",
+    top_growthing_stocks: str = "top_growthing_stocks.json",
+    top_profiting_stocks: str = "top_profiting_stocks.json",
+    queue_cache: int = 10,
+    max_workers: int = None,
+    print_stock: bool = False,
+):
     """
-    Takes `path` to the save directory and:
+    Execute `StocksParser` and saves to the `directory`:
 
-    + Writes all parsed data by `StocksParser` to `all_data.json` file.
+    + `top_low_ratio_stocks` - The lowest P/E ratio stocks.
+    Name is `"top_low_ratio_stocks.json"` by default.
 
-    Note:
-    -----
-    `top_ten_*` results sort in ascending order.
+    + `top_expensive_stocks` - The most expensive stocks with prices in RUB.
+    Name is `"top_expensive_stocks.json"` by default.
 
-    + Writes top ten the most expensive stocks with prices in
-    RUB to `top_ten_expensive_stocks.json` file in the format
+    + `top_growthing_stocks` - The most growing stocks at last year in percentage.
+    Name is `"top_expensive_stocks.json"` by default.
+
+    + `top_profiting_stocks` - The most potentially profiting stocks.
+    Name is `"top_expensive_stocks.json"` by default.
+
+    in JSON format
     ```
-    {"code": str, "name": str, "price" float}
-    ```
-
-    + Writes top ten the lowest P/E ratio stocks to
-    `top_ten_low_ratio_stocks.json` file in the format
-    ```
-    {"code": str, "name": str, "P/E" float}
+    [{"ratio" | "price" | "growth" | "profit": float, "name": str, "code": str}]
     ```
 
-    + Writes top ten the most growing stocks at last year in
-    percentage to `top_ten_growthing_stocks.json` file in the format
-    ```
-    {"code": str, "name": str, "growth" float}
-    ```
-
-    + Writes top ten the most potentially profiting stocks in
-    percentage to `top_ten_profiting_stocks.json` file in the format
-    ```
-    {"code": str, "name": str, "potential profit" float}
-    ```
-
-    By default `encoding` is "UTF-8".
+    Default key-value arguments:
+    ----------------------------
+    + `queue_cache` - Number of stocks to write. `10` by default.
+    + `max_workers` - Max number of threads for pages parsing.
+    `None` by default i.e. number of pagination pages.
+    + `print_stock` - Prints whole raw stock info in StdOut.
+    `False` by default.
     """
-    directory = Path(path)
+    directory = Path(directory)
+    directory.mkdir(exist_ok=True)
 
-    all_data_json = directory.joinpath(ALL_DATA_STOCKS)
-    dump_as_json(StocksParser.execute(), all_data_json)
+    parsed_stocks = StocksParser(
+        queue_cache=queue_cache,
+        max_workers=max_workers,
+        print_stock=print_stock,
+    )
+    parsed_stocks.execute()
 
-    with open(file=all_data_json, mode="r", encoding=encoding) as as_json:
-        expensive, low_ratio, growthing, profiting = keep_top_tens(load(as_json))
-
-    dump_as_json(expensive, directory.joinpath(TOP_TEN_EXPENSIVE_STOCKS))
-    dump_as_json(low_ratio, directory.joinpath(TOP_TEN_LOW_RATIO_STOCKS))
-    dump_as_json(growthing, directory.joinpath(TOP_TEN_GROWTHING_STOCKS))
-    dump_as_json(profiting, directory.joinpath(TOP_TEN_PROFITING_STOCKS))
-
-
-def dump_as_json(data: List[Dict], path: Path, encoding="UTF-8"):
-    """
-    Writes `data` to json file to `path`. By default `encoding` is "UTF-8".
-    """
-    with open(file=path, mode="w", encoding=encoding) as as_json:
-        dump(data, as_json, indent=4)
-
-
-def keep_top_tens(stocks: List[Dict]) -> Tuple[List[Dict]]:
-    """
-    Takes `stocks` and returns `Tuple` with four `List`s
-    (sort in ascending order):
-
-    + Top ten the most expensive stocks with prices
-    in RUB in the format
-    ```
-    {"code": str, "name": str, "price" float}
-    ```
-
-    + Top ten the lowest P/E ratio stocks
-    in the format
-    ```
-    {"code": str, "name": str, "P/E" float}
-    ```
-
-    + Top ten the most growing stocks at last year
-    in percentage in the format
-    ```
-    {"code": str, "name": str, "growth" float}
-    ```
-
-    + Writes top ten the most potentially profiting stocks
-    in percentage in the format
-    ```
-    {"code": str, "name": str, "potential profit" float}
-    ```
-    """
-    expensive, low_ratio, growthing, profiting = [], [], [], []
-
-    for stock in stocks:
-        _filter_queue(PRICE, stock, expensive, lambda queue: min(queue, key=_key_price))
-        if stock[RATIO]:
-            _filter_queue(
-                RATIO, stock, low_ratio, lambda queue: max(queue, key=_key_ratio)
-            )
-        _filter_queue(
-            GROWTH, stock, growthing, lambda queue: min(queue, key=_key_growth)
-        )
-        _filter_queue(
-            PROFIT, stock, profiting, lambda queue: min(queue, key=_key_profit)
-        )
-
-    expensive.sort(key=_key_price)
-    low_ratio.sort(key=_key_ratio)
-    growthing.sort(key=_key_growth)
-    profiting.sort(key=_key_profit)
-
-    return expensive, low_ratio, growthing, profiting
-
-
-def _filter_queue(key: str, stock: Dict, queue: List, remove_filter: Callable):
-    """
-    Adds key-value from `stock` according to `key`.
-    Maintains `queue` according to `filter_` invariant.
-
-    Arguments:
-    + `key` - necessary key for `stock`.
-    + `stock` - current stock entry.
-    + `queue` - a list which keeps queue with length 10 according to `filter_`.
-    + `remove_filter` - function which keeps queue invariant.
-    """
-    queue.append({CODE: stock[CODE], NAME: stock[NAME], key: stock[key]})
-    if len(queue) > 10:
-        queue.remove(remove_filter(queue))
-
-
-def _key_price(stock: Dict) -> float:
-    """
-    Returns value from `stock` for `PRICE` key.
-    """
-    return stock[PRICE]
-
-
-def _key_ratio(stock: Dict) -> float:
-    """
-    Returns value from `stock` for `RATIO` key.
-    """
-    return stock[RATIO]
-
-
-def _key_growth(stock: Dict) -> float:
-    """
-    Returns value from `stock` for `GROWTH` key.
-    """
-    return stock[GROWTH]
-
-
-def _key_profit(stock: Dict) -> float:
-    """
-    Returns value from `stock` for `PROFIT` key.
-    """
-    return stock[PROFIT]
+    parsed_stocks.dump_low_ratio(directory / top_low_ratio_stocks)
+    parsed_stocks.dump_expensive(directory / top_expensive_stocks)
+    parsed_stocks.dump_growthing(directory / top_growthing_stocks)
+    parsed_stocks.dump_profiting(directory / top_profiting_stocks)
 
 
 if __name__ == "__main__":
-    main("homework_10")
+    main("homework_10/data/")
